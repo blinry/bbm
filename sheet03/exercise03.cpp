3f21e03706e8cc2287267102d89f045e60297532
Sebastian Morr <sebastian@morr.cc>
2015-05-19 19:05:37 +0200

    Even more fucking errors

--- sheet03/exercise03.cpp
+++ sheet03/exercise03.cpp
@@ -21,7 +21,7 @@ using namespace cv;
 using namespace std;
 
 #define WINDOW_SIZE 10
-#define PI 180
+#define HOUGH_SIZE 600
 
 /**
  * Aufgabe: Median-Filter (10 Punkte)
@@ -46,14 +46,14 @@ Mat median_filter(Mat img, int windowSize){
 			for(int x = (-windowSize/2); x < windowSize/2; x++){
 				for(int y = (-windowSize/2); y < windowSize/2; y++){
 					if (i+x >= 0 && i+x < img.cols && j+y >= 0 && j+y < img.rows) {
-						window.push_back( img.at<float>(Point2i(i+x,j+y)) );
+						window.push_back( img.at<float>(j+y, i+x) );
 					}
 				}
 			}
 			// 2.2
 			sort(window.begin(), window.end());
 // 			// 2.3
-			transformedImage.at<float>(Point2i(i,j)) = window[window.size()/2];
+			transformedImage.at<float>(j, i) = window[window.size()/2];
 		}
 	}
 	// Steps:
@@ -65,51 +65,75 @@ Mat median_filter(Mat img, int windowSize){
 	 * 	2.3 - set the median pixel as the value in the corresponding position in the transformed image
 	 */
 	
-	/*
-	 * transformedImage.at(Point pt) = median
-	 */
-	
 	return transformedImage;
 }
 
-Mat hough_tranformation(Mat img){
-	
-// 	int max_d = sqrt(exp2(img.rows/2) + exp2(img.cols/2));
-	int max_d = sqrt(pow(img.rows/2, 2) + pow(img.cols/2, 2));
-// 	int min_d = -max_d;
-	
-// 	cout << img.rows << "\n";
-	cout << "max d: " << max_d << "\n";
-	
-	Mat hough = Mat::zeros(PI, 2*max_d, CV_8UC1);
-// 	Mat hough = Mat::zeros(42, 42, CV_8UC1);
-	
-	for(int i = 0; i < img.cols; i++){
-		for(int j = 0; j < img.rows; j++){
-			if(img.at<float>(Point2i(i,j)) != 0){
-				for(int alpha = 0; alpha < PI; alpha++){
-					float alpha_radian = alpha*M_PI/PI;
-					int d = i*cos(alpha_radian) + j*sin(alpha_radian) + max_d - 1;
-					cout << "d: " << d << " ; ";
-					cout << "alpha: " << alpha << "\n";
-					hough.at<uint>(Point2i(alpha, d))++;
-				}
-			}
-		}
-	}
-	
-	return hough;
-// 	max_d := sqrt((1/2 * bildhöhe)^2 + (1/2 * bildbreite)^2)
-// 	min_d := max_d * -1
-// 	houghRaum[0...\pi][min_d...max_d] := 0
-// 	foreach pixel != 0 do
-// 		for \alpha := 0 to \pi do
-// 		d := pixelx * cos(\alpha) + pixely * sin(\alpha)
-// 		houghRaum[\alpha][d]++
-// 		end
-// 	end
-}
+//Mat hough_tranformation_2(Mat img){
+//// 	int max_d = sqrt(exp2(img.rows/2) + exp2(img.cols/2));
+//	int max_d = sqrt(pow(img.rows, 2) + pow(img.cols, 2));
+//// 	int min_d = -max_d;
+//	
+//	Mat hough = Mat::zeros(HOUGH_SIZE, 2*max_d, CV_32FC1);
+//	
+//	for(int i = 0; i < img.cols; i++){
+//		for(int j = 0; j < img.rows; j++){
+//			if(img.at<float>(j, i) != 0){
+//				for(int alpha = 0; alpha < HOUGH_SIZE; alpha++){
+//					float alpha_radian = 1.0*alpha/HOUGH_SIZE*M_PI;
+//                    //cout << "ar: " << alpha_radian << endl;
+//					int d = (i)*cos(alpha_radian) + (j)*sin(alpha_radian) + max_d;
+//					//cout << "d: " << d << endl;
+//                    if (d<0) {
+//                        cout << d << endl;
+//                        cout << "n1o" << endl;
+//                    }
+//                    if (d > 2*max_d-1) {
+//                        cout << d << endl;
+//                        cout << "n2o" << endl;
+//                    }
+//                    if (alpha < 0) {
+//                        cout << "n3o" << endl;
+//                    }
+//                    if (alpha > HOUGH_SIZE-1) {
+//                        cout << "n4o" << endl;
+//                    }
+//					hough.at<float>(alpha, d) += 0.003;
+//				}
+//			}
+//		}
+//	}
+//	
+//    //hough.convertTo(hough, CV_32FC1, 1/10);
+//	return hough;
+//// 	max_d := sqrt((1/2 * bildhöhe)^2 + (1/2 * bildbreite)^2)
+//// 	min_d := max_d * -1
+//// 	houghRaum[0...\pi][min_d...max_d] := 0
+//// 	foreach pixel != 0 do
+//// 		for \alpha := 0 to \pi do
+//// 		d := pixelx * cos(\alpha) + pixely * sin(\alpha)
+//// 		houghRaum[\alpha][d]++
+//// 		end
+//// 	end
+//}
 
+Mat hough_tranformation(Mat img){
+    int max_d = sqrt(img.cols*img.cols + img.rows*img.rows);
+    Mat hough(HOUGH_SIZE, 2*max_d, CV_32FC1);
+
+    for (int x=0; x < img.rows; x++) {
+        for (int y=0; y < img.cols; y++) {
+            if (img.at<float>(x,y) > 0) {
+                for (int a=0; a < HOUGH_SIZE; a++) {
+                    float alpha = 1.0*a/HOUGH_SIZE*M_PI;
+                    float d = x*cos(alpha) + y*sin(alpha) + max_d;
+                    hough.at<float>(a,d) += 0.01;
+                }
+            }
+        }
+    }
+
+    return hough;
+}
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -117,45 +141,42 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // load the input image
-//     IplImage *img = cvLoadImage(argv[1]);
-
     /**
      * - Wende den Median-Filter auf ein Graustufenbild an.
      */
 
 	Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-// 	img.convertTo(img, CV_32FC1, 1.0/255);
-// 	Mat medianImg = median_filter(img, WINDOW_SIZE);
-// 	namedWindow("Median Filter");
-// 	imshow("Median Filter", medianImg);
-// 	waitKey(0);
+ 	img.convertTo(img, CV_32FC1, 1.0/255);
+ 	Mat medianImg = median_filter(img, WINDOW_SIZE);
+ 	namedWindow("Median Filter");
+ 	imshow("Median Filter", medianImg);
+ 	waitKey(0);
 
 
     /**
      * - Wende den Median-Filter auf die einzelnen Kanäle eines Farbbilds an
      */
 
-// 	Mat channels[3];
-// 	Mat median_channels[3];
-// 	img = imread(argv[1]);
-// 	img.convertTo(img, CV_32FC3, 1.0/255);
-// 	split(img, channels);
-// 	for (int i = 0; i < 3; i++){
-// 		median_channels[i] = median_filter(channels[i], WINDOW_SIZE);
-// 	}
-// 	namedWindow("Median Filter Colour 1");
-// 	imshow("Median Filter Colour 1", median_channels[0]);
-// 	waitKey(0);
-// 	imshow("Median Filter Colour 1", median_channels[1]);
-// 	waitKey(0);
-// 	imshow("Median Filter Colour 1", median_channels[2]);
-// 	waitKey(0);
-// 	
-// 	Mat merged_channels(img.rows, img.cols, CV_32FC3);
-// 	merge(median_channels, 3, merged_channels);
-// 	imshow("Median Filter Colour 1", merged_channels);
-// 	waitKey(0);
+ 	Mat channels[3];
+ 	Mat median_channels[3];
+ 	img = imread(argv[1]);
+ 	img.convertTo(img, CV_32FC3, 1.0/255);
+ 	split(img, channels);
+ 	for (int i = 0; i < 3; i++){
+ 		median_channels[i] = median_filter(channels[i], WINDOW_SIZE);
+ 	}
+ 	namedWindow("Median Filter Colour 1");
+ 	imshow("Median Filter Colour 1", median_channels[0]);
+ 	waitKey(0);
+ 	imshow("Median Filter Colour 1", median_channels[1]);
+ 	waitKey(0);
+ 	imshow("Median Filter Colour 1", median_channels[2]);
+ 	waitKey(0);
+ 	
+ 	Mat merged_channels(img.rows, img.cols, CV_32FC3);
+ 	merge(median_channels, 3, merged_channels);
+ 	imshow("Median Filter Colour 1", merged_channels);
+ 	waitKey(0);
 
 
     /**
@@ -171,41 +192,67 @@ int main(int argc, char **argv) {
      * - Lade ein Testbild und wandle es in Graustufen um.
      */
 
-    /* TODO */
 	img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	img.convertTo(img, CV_32FC1, 1.0/255);
-	Mat houghImg = hough_tranformation(img);
-	namedWindow("Hough Filter");
-	imshow("Hough Filter", houghImg);
-	waitKey(0);
 
 
     /**
      * - Erzeuge ein Gradientenbild. Verwende dazu einen Filter deiner Wahl. Begründe die Wahl des Kantendetektors.
      */
 
-    /* TODO */
+    Mat edge = img.clone();
+    Canny(img, edge, 200, 500);
+    
+    namedWindow("Hough Filter");
+    imshow("Hough Filter", edge);
+    waitKey(0);
 
 
     /**
      * - Transformiere das Gradientenbild in den Hough-Raum und zeige das Bild an.
      */
-//     IplImage *hough = cvCreateImage(cvSize(400, 400), IPL_DEPTH_32F, 1);
 
-    /* TODO */
+    Mat houghImg = hough_tranformation(edge);
 
-//     cvNamedWindow("Hough Space"); 
-//     cvShowImage("Hough Space", hough);
-//     cvWaitKey(0);
+    namedWindow("Hough Filter");
+    imshow("Hough Filter", houghImg);
+    waitKey(0);
+
+    Mat filteredHough(houghImg.rows, houghImg.cols, houghImg.type());
+    threshold(houghImg, filteredHough, 1.5, 1, THRESH_BINARY);
+
+    namedWindow("Hough Filter");
+    imshow("Hough Filter", filteredHough);
+    waitKey(0);
+
 
     /**
      * - Finde die markantesten Linien und zeichne diese in das Originalbild ein.
      */
 
-    /* TODO */
+    int max_d = sqrt(img.rows*img.rows + img.cols*img.cols);
 
-//     cvNamedWindow("Hough Lines");
-//     cvShowImage("Hough Lines", img);
-//     cvWaitKey(0); 
+    for(int d = 0; d < filteredHough.cols; d++){
+        for(int a = 0; a < filteredHough.rows; a++){
+            if (filteredHough.at<float>(a,d) == 1) {
+                float alpha = 1.0*a/HOUGH_SIZE*M_PI;
+                int real_d = d - max_d;
+
+                int x = real_d*cos(M_PI/2.0-alpha);
+                int y = real_d*sin(M_PI/2.0-alpha);
+                int x2 = 0;
+                int y2 = 1.0*real_d/cos(alpha);
+
+                line(img, Point(x2,y2), Point(x,y), Scalar(255, 255, 255));
+                Point mid(x,y);
+                Point shift(5,5);
+                Rect rect(mid-shift, mid+shift);
+                rectangle(img, rect, 0);
+            }
+        }
+    }
+
+    namedWindow("Hough Filter");
+    imshow("Hough Filter", img);
+    waitKey(0);
 }
 
