697c2a01f74a4f177a9d731c7e9e816efb6fe5ef
Sebastian Morr <sebastian@morr.cc>
2015-05-21 10:26:43 +0200

    Comments

--- sheet03/exercise03.cpp
+++ sheet03/exercise03.cpp
@@ -31,11 +31,7 @@ using namespace std;
  * - Implementiere einen Median-Filter, ohne cvSmooth zu verwenden.
  */
 
-/* TODO */
-
 Mat median_filter(Mat img, int windowSize){
-	
-	//Mat *originalImage = new Mat(img);
 	Mat transformedImage(img.rows, img.cols, CV_32FC1);
 	vector<float> window;
 	
@@ -52,72 +48,28 @@ Mat median_filter(Mat img, int windowSize){
 			}
 			// 2.2
 			sort(window.begin(), window.end());
-// 			// 2.3
+			// 2.3
 			transformedImage.at<float>(j, i) = window[window.size()/2];
 		}
 	}
-	// Steps:
-	/* 
-	 * 1 - Create a Window with size windowSize
-	 * 2- Slide with the window over the original image.
-	 * 	2.1 - get all the pixels in that area
-	 * 	2.2 - calculate the median
-	 * 	2.3 - set the median pixel as the value in the corresponding position in the transformed image
-	 */
 	
 	return transformedImage;
 }
 
-//Mat hough_tranformation_2(Mat img){
-//// 	int max_d = sqrt(exp2(img.rows/2) + exp2(img.cols/2));
-//	int max_d = sqrt(pow(img.rows, 2) + pow(img.cols, 2));
-//// 	int min_d = -max_d;
-//	
-//	Mat hough = Mat::zeros(HOUGH_SIZE, 2*max_d, CV_32FC1);
-//	
-//	for(int i = 0; i < img.cols; i++){
-//		for(int j = 0; j < img.rows; j++){
-//			if(img.at<float>(j, i) != 0){
-//				for(int alpha = 0; alpha < HOUGH_SIZE; alpha++){
-//					float alpha_radian = 1.0*alpha/HOUGH_SIZE*M_PI;
-//                    //cout << "ar: " << alpha_radian << endl;
-//					int d = (i)*cos(alpha_radian) + (j)*sin(alpha_radian) + max_d;
-//					//cout << "d: " << d << endl;
-//                    if (d<0) {
-//                        cout << d << endl;
-//                        cout << "n1o" << endl;
-//                    }
-//                    if (d > 2*max_d-1) {
-//                        cout << d << endl;
-//                        cout << "n2o" << endl;
-//                    }
-//                    if (alpha < 0) {
-//                        cout << "n3o" << endl;
-//                    }
-//                    if (alpha > HOUGH_SIZE-1) {
-//                        cout << "n4o" << endl;
-//                    }
-//					hough.at<float>(alpha, d) += 0.003;
-//				}
-//			}
-//		}
-//	}
-//	
-//    //hough.convertTo(hough, CV_32FC1, 1/10);
-//	return hough;
-//// 	max_d := sqrt((1/2 * bildhöhe)^2 + (1/2 * bildbreite)^2)
-//// 	min_d := max_d * -1
-//// 	houghRaum[0...\pi][min_d...max_d] := 0
-//// 	foreach pixel != 0 do
-//// 		for \alpha := 0 to \pi do
-//// 		d := pixelx * cos(\alpha) + pixely * sin(\alpha)
-//// 		houghRaum[\alpha][d]++
-//// 		end
-//// 	end
-//}
+/**
+ * Aufgabe: Hough-Transformation (10 Punkte)
+ *
+ * Die Hough-Transformation kann für das Finden von Linien in Bildern verwendet werden.
+ *
+ * In dieser Aufgabe sollst du die Hough-Transformation implementieren.
+ * - Lade ein Testbild und wandle es in Graustufen um.
+ */
 
 Mat hough_tranformation(Mat img){
+    // Maximum possible distance of a pixel to the origin at (0,0):
     int max_d = sqrt(img.cols*img.cols + img.rows*img.rows);
+
+    // Distance can also be negative:
     Mat hough(HOUGH_SIZE, 2*max_d, CV_32FC1);
 
     for (int x=0; x < img.rows; x++) {
@@ -125,6 +77,7 @@ Mat hough_tranformation(Mat img){
             if (img.at<float>(x,y) > 0) {
                 for (int a=0; a < HOUGH_SIZE; a++) {
                     float alpha = 1.0*a/HOUGH_SIZE*M_PI;
+                    // Shift d by max_d to make it always positive:
                     float d = x*cos(alpha) + y*sin(alpha) + max_d;
                     hough.at<float>(a,d) += 0.01;
                 }
@@ -162,20 +115,23 @@ int main(int argc, char **argv) {
  	img = imread(argv[1]);
  	img.convertTo(img, CV_32FC3, 1.0/255);
  	split(img, channels);
+
  	for (int i = 0; i < 3; i++){
  		median_channels[i] = median_filter(channels[i], WINDOW_SIZE);
  	}
- 	namedWindow("Median Filter Colour 1");
- 	imshow("Median Filter Colour 1", median_channels[0]);
+
+ 	namedWindow("Median Filter single channel");
+ 	imshow("Median Filter single channel", median_channels[0]);
  	waitKey(0);
- 	imshow("Median Filter Colour 1", median_channels[1]);
+ 	imshow("Median Filter single channel", median_channels[1]);
  	waitKey(0);
- 	imshow("Median Filter Colour 1", median_channels[2]);
+ 	imshow("Median Filter single channel", median_channels[2]);
  	waitKey(0);
  	
  	Mat merged_channels(img.rows, img.cols, CV_32FC3);
  	merge(median_channels, 3, merged_channels);
- 	imshow("Median Filter Colour 1", merged_channels);
+    namedWindow("Median Filter merged channels");
+ 	imshow("Median Filter merged channels", merged_channels);
  	waitKey(0);
 
 
@@ -183,29 +139,19 @@ int main(int argc, char **argv) {
      * - Wie kann man ungewollte Farbverschiebungen vermeiden?
      */
 
-    /**
-     * Aufgabe: Hough-Transformation (10 Punkte)
-     *
-     * Die Hough-Transformation kann für das Finden von Linien in Bildern verwendet werden.
-     *
-     * In dieser Aufgabe sollst du die Hough-Transformation implementieren.
-     * - Lade ein Testbild und wandle es in Graustufen um.
-     */
-
-	img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-
+    // Für die drei Channels jeweils den gleichen Pixel nehmen.
 
     /**
      * - Erzeuge ein Gradientenbild. Verwende dazu einen Filter deiner Wahl. Begründe die Wahl des Kantendetektors.
      */
 
+    img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
     Mat edge = img.clone();
     Canny(img, edge, 200, 500);
-    
+
     namedWindow("Hough Filter");
     imshow("Hough Filter", edge);
     waitKey(0);
-
 
     /**
      * - Transformiere das Gradientenbild in den Hough-Raum und zeige das Bild an.
@@ -235,6 +181,8 @@ int main(int argc, char **argv) {
         for(int a = 0; a < filteredHough.rows; a++){
             if (filteredHough.at<float>(a,d) == 1) {
                 float alpha = 1.0*a/HOUGH_SIZE*M_PI;
+
+                // Shift the d back
                 int real_d = d - max_d;
 
                 int x = real_d*cos(M_PI/2.0-alpha);
