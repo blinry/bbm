a4737dfd3ea7f19d659ffb86aaf7f25cf3395a0c
Sebastian Morr <sebastian@morr.cc>
2015-06-04 19:12:05 +0200

    Solutions, except for the correct final image size

--- sheet04/exercise04.cpp
+++ sheet04/exercise04.cpp
@@ -16,8 +16,39 @@
 #include <cv.h>
 #include <highgui.h>
 
-template<typename T> const T &min(const T &a, const T &b) { return a < b ? a : b; }
-template<typename T> const T &max(const T &a, const T &b) { return a > b ? a : b; }
+using namespace cv;
+
+//template<typename T> const T &min(const T &a, const T &b) { return a < b ? a : b; }
+//template<typename T> const T &max(const T &a, const T &b) { return a > b ? a : b; }
+
+Point2f operator*(Mat M, const Point2f& p){
+    Mat src(3, 1, CV_32FC1);
+
+    src.at<float>(0,0)=p.x;
+    src.at<float>(1,0)=p.y;
+    src.at<float>(2,0)=1.0;
+
+    Mat dst = M*src;
+    return Point2f(dst.at<float>(0,0)/dst.at<float>(2,0),dst.at<float>(1,0)/dst.at<float>(2,0));
+}
+
+Mat get_translation_matrix(int dx, int dy) {
+    Mat translation = Mat::zeros(3, 3, CV_32FC1);
+    translation.at<float>(0, 0) = 1;
+    translation.at<float>(1, 1) = 1;
+    translation.at<float>(0, 2) = dx;
+    translation.at<float>(1, 2) = dy;
+    translation.at<float>(2, 2) = 1;
+    return translation;
+}
+
+Mat translate(Mat img, int dx, int dy) {
+    Mat translation = get_translation_matrix(dx, dy);
+    Mat imgt(img.rows, img.cols, CV_32F);
+    warpPerspective(img, imgt, translation, imgt.size());
+    return imgt;
+}
+
 
 int main(int argc, char *argv[]) {
 	if (argc < 3){
@@ -25,23 +56,24 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	IplImage* img1 = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
-	if (!img1) {
-		printf("Could not load image file: %s\n", argv[1]);
-		exit(1);
-	}
-	IplImage* img1f = cvCreateImage(cvGetSize(img1), IPL_DEPTH_32F, 1);
-	cvConvertScale(img1, img1f, 1.0 / 255.0);
+    Mat img1 = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    img1.convertTo(img1, CV_32FC1, 1.0/255);
 
-	IplImage* img2 = cvLoadImage(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
-	if (!img2) {
-		printf("Could not load image file: %s\n", argv[2]);
-		exit(1);
-	}
-	IplImage* img2f = cvCreateImage(cvGetSize(img1), IPL_DEPTH_32F, 1);
-	cvConvertScale(img2, img2f, 1.0 / 255.0);
+    Mat img2 = imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+    img2.convertTo(img2, CV_32FC1, 1.0/255);
 
-	/**
+
+    /* 4.1.1 Save trees!
+     *
+     * | 1 0 a |   | x |   | x + a |
+     * | 0 1 b | * | y | = | y + b |
+     * | 0 0 1 |   | f |   |   f   |
+     *
+     * 4.1.3: Four.
+     *
+     */
+
+/*
 	 * Aufgabe: Homographien (5 Punkte)
 	 * 
 	 * Unter der Annahme, dass Bilder mit einer verzerrungsfreien Lochbildkamera
@@ -57,7 +89,15 @@ int main(int argc, char *argv[]) {
 	 *   bestimmen? Warum? (Schriftlich beantworten!)
 	 */
 
-/* TODO */
+    Mat img1t = translate(img1, 20, 0);
+
+    namedWindow("Original image");
+    imshow("Original image", img1);
+    waitKey(0);
+
+    namedWindow("Translated image");
+    imshow("Translated image", img1t);
+    waitKey(0);
 
 
 	/**
@@ -77,7 +117,7 @@ int main(int argc, char *argv[]) {
 	 * $(x, y)$ & $(x, y)$ \\ \hline \hline
 	 * (463, 164) & (225, 179)\\ \hline
 	 * (530, 357) & (294, 370)\\ \hline
-	 * (618, 357) &(379, 367)\\ \hline
+	 * (618, 357) & (379, 367)\\ \hline
 	 * (610, 153) & (369, 168)\\ \hline
 	 * \end{tabular}
 	 * \end{center}
@@ -89,38 +129,79 @@ int main(int argc, char *argv[]) {
 	 *   wurde?
 	*/
 
-/* TODO */
+    Point2f points2[4] = {Point2f(463, 164), Point2f(530, 357), Point2f(618, 357), Point2f(610, 153)};
+    Point2f points1[4] = {Point2f(225, 179), Point2f(294, 370), Point2f(379, 367), Point2f(369, 168)};
 
+    Mat transformation = getPerspectiveTransform(points1, points2);
+    transformation.convertTo(transformation, CV_32FC1, 1);
+
+    Point2f topleft(0, 0);
+    Point2f topright(img2.cols-1, 0);
+    Point2f bottomleft(0, img2.rows-1);
+    Point2f bottomright(img2.cols-1, img2.rows-1);
+
+    Point2f topleft_t = transformation*topleft;
+    Point2f topright_t = transformation*topright;
+    Point2f bottomleft_t = transformation*bottomleft;
+    Point2f bottomright_t = transformation*bottomright;
+
+    int xmax = max(topright_t.x, bottomright_t.x);
+    int xmin = min(topleft_t.x, bottomleft_t.x);
+    int ymax = max(bottomright_t.y, bottomleft_t.y);
+    int ymin = min(topright_t.y, topleft_t.y);
+
+    std::cout << xmax << std::endl;
+    std::cout << ymax << std::endl;
+    std::cout << xmin << std::endl;
+    std::cout << ymin << std::endl;
+
+    //Mat both = Mat::zeros(ymax-ymin, xmax-xmin, CV_32F);
 	
 	/**
 	 * - Bestimme die notwendige Bildgröße für das Panoramabild.
 	 */
 
-/* TODO */
+    /**
+     * - Projiziere das linke Bild in die Bildebene des rechten Bildes. Beachte
+     *   dabei, dass auch der linke Bildrand in das Panoramabild projiziert
+     *   wird.
+     */
 
+    Mat both1(ymax-ymin, xmax, CV_32F);
+    Mat both2 = both1.clone();
 
-	/**
-	 * - Projiziere das linke Bild in die Bildebene des rechten Bildes. Beachte
-	 *   dabei, dass auch der linke Bildrand in das Panoramabild projiziert
-	 *   wird.
-	 */
+    Mat translation = get_translation_matrix(0, -ymin);
 
-/* TODO */
+    warpPerspective(img1, both1, translation, both1.size());
+    warpPerspective(img2, both2, transformation*translation, both2.size());
 
+    /**
+     * - Bilde das Panoramabild, so dass Pixel, für die zwei Werte vorhanden sind,
+     *   den Mittelwert zugeordnet bekommen.
+     */
 
-	/**
-	 * - Bilde das Panoramabild, so dass Pixel, für die zwei Werte vorhanden sind,
-	 *   den Mittelwert zugeordnet bekommen.
-	 */
+    Mat both = both1.clone();
 
-/* TODO */
-
+    for (int x=0; x < both1.cols; x++) {
+        for (int y=0; y < both1.rows; y++) {
+            if (both1.at<float>(y,x) != 0) {
+                if (both2.at<float>(y,x) != 0) {
+                    both.at<float>(y,x) = (both1.at<float>(y,x)+both2.at<float>(y,x))/2.0;
+                } else {
+                    both.at<float>(y,x) = both1.at<float>(y,x);
+                }
+            } else {
+                both.at<float>(y,x) = both2.at<float>(y,x);
+            }
+        }
+    }
 
 	/**
 	 * - Zeige das Panoramabild an.
 	 */
 
-/* TODO */
-
+    namedWindow("Full image");
+    imshow("Full image", both);
+    waitKey(0);
 }
 
